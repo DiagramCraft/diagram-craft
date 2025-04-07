f84abf8d5032a7f71c03b50b4d0f843a557a8487
Magnus Johansson <kkckkc@gmail.com>
2025-04-07 15:26:34 +0200

    feat[#154]: initial boolean operations implementation

---
+++ packages/geometry/src/pathClip.ts
@@ -0,0 +1,363 @@
+import { CompoundPath } from './pathBuilder';
+import { Point } from './point';
+import { PathSegment } from './pathSegment';
+import { assert, VERIFY_NOT_REACHED, VerifyNotReached } from '@diagram-craft/utils/assert';
+import { Path } from './path';
+
+type VertexType = 'in->out' | 'out->in';
+
+type Vertex = {
+  point: Point;
+  segment: PathSegment;
+  alpha: number;
+
+  prev: Vertex;
+  next: Vertex;
+} & (
+  | { intersect: true; neighbor: Vertex; type?: VertexType }
+  | { intersect: false; neighbor?: never; type?: VertexType }
+);
+
+// @ts-expect-error placeholder value to handle partial contstruction
+const SENTINEL_VERTEX: Vertex = {
+  point: { x: -Infinity, y: -Infinity }
+};
+SENTINEL_VERTEX.prev = SENTINEL_VERTEX;
+SENTINEL_VERTEX.next = SENTINEL_VERTEX;
+
+export type BooleanOperations =
+  | 'A union B'
+  | 'A not B'
+  | 'B not A'
+  | 'A intersection B'
+  | 'A xor B'
+  | 'A divide B';
+
+/*
+ * The implementation is based on https://www.inf.usi.ch/hormann/papers/Greiner.1998.ECO.pdf
+ */
+
+export const applyBooleanOperation = (
+  a: CompoundPath,
+  b: CompoundPath,
+  operation: BooleanOperations
+): CompoundPath => {
+  const vertices = getClipVertices(a, b);
+
+  switch (operation) {
+    case 'A union B':
+      classifyClipVertices(vertices, [a, b], [false, false]);
+      return clipVertices(vertices);
+    case 'A not B':
+      classifyClipVertices(vertices, [a, b], [false, true]);
+      return clipVertices(vertices);
+    case 'B not A':
+      classifyClipVertices(vertices, [a, b], [true, false]);
+      return clipVertices(vertices);
+    case 'A intersection B':
+      classifyClipVertices(vertices, [a, b], [true, true]);
+      return clipVertices(vertices);
+    case 'A xor B': {
+      const cp1 = applyBooleanOperation(a, b, 'A not B');
+      const cp2 = applyBooleanOperation(a, b, 'B not A');
+      return new CompoundPath([...cp1.all(), ...cp2.all()]);
+    }
+    case 'A divide B': {
+      return new CompoundPath([
+        ...applyBooleanOperation(a, b, 'A xor B').all(),
+        ...applyBooleanOperation(a, b, 'A intersection B').all()
+      ]);
+    }
+    default:
+      throw new VerifyNotReached();
+  }
+};
+
+const clipSegments = (vertices: Vertex[]) => {
+  for (let i = 0; i < vertices.length; i++) {
+    const current = vertices[i];
+
+    const clips: Vertex[] = [];
+    for (let j = i + 1; j < vertices.length; j++) {
+      if (!vertices[j].intersect) break;
+      clips.push(vertices[j]);
+    }
+
+    if (clips.length === 0) continue;
+
+    clips.reverse();
+
+    let remaining = current.segment;
+
+    for (const c of clips) {
+      const clipped = current.segment.split(c.alpha);
+      remaining = clipped[0];
+      c.segment = clipped[1];
+    }
+
+    current.segment = remaining;
+  }
+};
+
+const makeLinkedList = (vertices: Vertex[]) => {
+  for (let i = 0; i < vertices.length; i++) {
+    vertices[i].next = vertices[(i + 1) % vertices.length];
+    vertices[i].prev = vertices[i === 0 ? vertices.length - 1 : i - 1];
+  }
+};
+
+const sortIntoVertexList = (p: CompoundPath, intersectionVertices: Map<PathSegment, Vertex[]>) => {
+  const dest: Vertex[] = [];
+  for (const s of p.singularPath().segments) {
+    dest.push({
+      point: s.start,
+      segment: s,
+      alpha: 0,
+      intersect: false,
+      prev: SENTINEL_VERTEX,
+      next: SENTINEL_VERTEX
+    });
+    dest.push(...(intersectionVertices.get(s) ?? []).sort((a, b) => a.alpha - b.alpha));
+  }
+  return dest;
+};
+
+const arrangeSegments = (dest: Vertex[][]) => {
+  const paths: PathSegment[][] = [];
+
+  for (const contour of dest) {
+    const currentPath: PathSegment[] = [];
+    for (let i = 0; i < contour.length - 1; i++) {
+      const current = contour[i];
+      const next = contour[i + 1];
+      if (current.next === next || current.next === next.neighbor) {
+        currentPath.push(current.segment);
+      } else if (current.prev === next) {
+        currentPath.push(next.segment.reverse());
+      } else if (current.prev === next.neighbor) {
+        currentPath.push(next.neighbor.segment.reverse());
+      } else {
+        VERIFY_NOT_REACHED();
+      }
+    }
+    paths.push(currentPath);
+  }
+  return paths;
+};
+
+/*
+  for each vertex Si of subject polygon do
+    for each vertex Cj of clip polygon do
+      if intersect(Si,Si+1,Cj,Cj+1,a,b)
+        I1 = CreateVertex(Si,Si+1,a)
+        I2 = CreateVertex(Cj,Cj+1,b)
+        link intersection points I1 and I2
+        sort I1 into subject polygon
+        sort I2 into clip polygon
+      end if
+    end for
+  end for
+ */
+export const getClipVertices = (cp1: CompoundPath, cp2: CompoundPath): [Vertex[], Vertex[]] => {
+  assert.true(cp1.all().length === 1);
+  assert.true(cp2.all().length === 1);
+
+  const intersectionVertices: Map<PathSegment, Vertex[]> = new Map();
+
+  for (const s of cp1.singularPath().segments) {
+    for (const c of cp2.singularPath().segments) {
+      const intersections = s.intersectionsWith(c) ?? [];
+      for (const intersection of intersections) {
+        const p1 = s.projectPoint(intersection);
+        const i1: Vertex = {
+          point: intersection,
+          segment: s,
+          alpha: p1.t,
+          intersect: true,
+          prev: SENTINEL_VERTEX,
+          next: SENTINEL_VERTEX,
+          neighbor: SENTINEL_VERTEX
+        };
+
+        const p2 = c.projectPoint(intersection);
+        const i2: Vertex = {
+          point: intersection,
+          segment: c,
+          alpha: p2.t,
+          intersect: true,
+          prev: SENTINEL_VERTEX,
+          next: SENTINEL_VERTEX,
+          neighbor: SENTINEL_VERTEX
+        };
+
+        i1.neighbor = i2;
+        i2.neighbor = i1;
+
+        if (!intersectionVertices.has(s)) intersectionVertices.set(s, []);
+        if (!intersectionVertices.has(c)) intersectionVertices.set(c, []);
+
+        intersectionVertices.get(s)!.push(i1);
+        intersectionVertices.get(c)!.push(i2);
+      }
+    }
+  }
+
+  // Sort into the target vertex lists
+  const subjectVertices = sortIntoVertexList(cp1, intersectionVertices);
+  const clipVertices = sortIntoVertexList(cp2, intersectionVertices);
+
+  // Fix linked list
+  makeLinkedList(subjectVertices);
+  makeLinkedList(clipVertices);
+
+  // Clip segments
+  clipSegments(subjectVertices);
+  clipSegments(clipVertices);
+
+  return [subjectVertices, clipVertices];
+};
+
+/*
+  for both polygons P do
+    if P0 inside other polygon
+      status = exit
+    else
+      status = entry
+    end if
+    for each vertex Pi of polygon do
+      if Pi->intersect then
+        Pi->entry_exit = status
+        toggle status
+      end if
+    end for
+  end for
+ */
+export const classifyClipVertices = (
+  intersections: [Array<Vertex>, Array<Vertex>],
+  paths: [CompoundPath, CompoundPath],
+  type: [boolean, boolean]
+) => {
+  assert.true(paths[0].all().length === 1);
+  assert.true(paths[1].all().length === 1);
+
+  // for both polygons P do
+  for (const i of [0, 1]) {
+    const p = paths[i].singularPath();
+    const p0 = p.segments[0].start;
+
+    /*
+      if P0 inside other polygon
+        status = exit
+      else
+        status = entry
+      end if
+     */
+    let status = paths[1 - i].singularPath().isInside(p0);
+    if (type[i]) status = !status;
+
+    // for each vertex Pi of polygon do
+    for (const intersection of intersections[i]) {
+      // if Pi->intersect then
+      if (intersection.intersect) {
+        // Pi->entry_exit = status
+        intersection.type = status ? 'in->out' : 'out->in';
+
+        // toggle status
+        status = !status;
+      }
+    }
+  }
+};
+
+/*
+  while unprocessed intersecting points in subject polygon
+    current = first unprocessed intersecting point of subject polygon
+    newPolygon
+    newVertex(current)
+    repeat
+      if current->entry
+        repeat
+          current = current->next
+          newVertex(current)
+        until current->intersect
+      else
+        repeat
+          current = current->prev
+          newVertex(current)
+        until current->intersect
+      end if
+      current = current->neighbor
+    until PolygonClosed
+  end while
+ */
+export const clipVertices = (p: [Array<Vertex>, Array<Vertex>]) => {
+  const [subject] = p;
+
+  let unprocessedIntersectingPoints = subject.filter(v => v.intersect);
+
+  const dest: Vertex[][] = [];
+
+  // while unprocessed intersecting points in subject polygon
+  while (unprocessedIntersectingPoints.length > 0) {
+    let current: Vertex = unprocessedIntersectingPoints[0];
+
+    // newPolygon
+    const currentContour: Vertex[] = [];
+    dest.push(currentContour);
+
+    // newVertex(current)
+    currentContour.push(current);
+
+    // repeat
+    //   ...
+    // until PolygonClosed
+    do {
+      unprocessedIntersectingPoints = unprocessedIntersectingPoints.filter(v => v !== current);
+
+      let maxLoop = 1000;
+
+      if (current.type === 'in->out') {
+        // repeat
+        //   ...
+        // until current->intersect
+        do {
+          // current = current->next
+          current = current.next;
+          if (current.intersect) break;
+
+          // newVertex(current)
+          currentContour.push(current);
+        } while (maxLoop-- > 0);
+      } else if (current.type === 'out->in') {
+        // repeat
+        //   ...
+        // until current->intersect
+        do {
+          // current = current->prev
+          current = current.prev;
+          if (current.intersect) break;
+
+          // newVertex(current)
+          currentContour.push(current);
+        } while (maxLoop-- > 0);
+      } else {
+        VERIFY_NOT_REACHED();
+      }
+
+      assert.false(maxLoop === 0);
+      assert.present(current.neighbor);
+
+      current = current.neighbor;
+      currentContour.push(current);
+    } while (dest.at(-1)![0] !== current);
+  }
+
+  return new CompoundPath(
+    arrangeSegments(dest).map(arr => {
+      return new Path(
+        arr[0].start,
+        arr.flatMap(s => s.raw())
+      );
+    })
+  );
+};
---
+++ packages/main/src/path/path.boolean.stories.tsx
@@ -0,0 +1,228 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { PathBuilder } from '@diagram-craft/geometry/pathBuilder';
+import { Scale, Translation } from '@diagram-craft/geometry/transform';
+import {
+  applyBooleanOperation,
+  classifyClipVertices,
+  getClipVertices
+} from '@diagram-craft/geometry/pathClip';
+import { Path } from '@diagram-craft/geometry/path';
+
+const BooleanTest = () => {
+  const p1 = PathBuilder.fromString(
+    'M 0.1865,0.0781 C 0.3899,0.1569,0.6487,-0.0614,0.8521,0.0174 L 1,1 L 0.2604,1 C 0.242,0.7695,-0.2645,0.4693,0.1865,0.0781'
+  );
+  const p2 = PathBuilder.fromString(
+    'M 0,0 L 0.7539,0 C 0.801,0.25,1.1308,0.2143,0.9424,1 C 0.7068,0.9601,0.2356,0.8802,0,0.8403 L 0,0'
+  );
+
+  p1.setTransform([new Translation({ x: -0.3, y: -0.3 }), new Scale(100, 100)]);
+  p2.setTransform([new Translation({ x: -0.6, y: -0.6 }), new Scale(100, 100)]);
+
+  const cp1 = p1.getPaths();
+  const cp2 = p2.getPaths();
+
+  const [subject, clip] = getClipVertices(cp1, cp2);
+
+  classifyClipVertices([subject, clip], [cp1, cp2], [false, false]);
+
+  const s1 = cp1.asSvgPath();
+  const s2 = cp2.asSvgPath();
+
+  const aUnionB = applyBooleanOperation(cp1, cp2, 'A union B');
+  const aNotB = applyBooleanOperation(cp1, cp2, 'A not B');
+  const bNotA = applyBooleanOperation(cp1, cp2, 'B not A');
+  const aIntersectionB = applyBooleanOperation(cp1, cp2, 'A intersection B');
+  const aXorB = applyBooleanOperation(cp1, cp2, 'A xor B');
+  const aDivideB = applyBooleanOperation(cp1, cp2, 'A divide B');
+
+  return (
+    <svg
+      width={600}
+      height={600}
+      viewBox={'-100 -120 600 620'}
+      style={{ border: '1px solid black' }}
+    >
+      {/* Main shape */}
+      <g>
+        <text x={0} y={-80} width={200} textAnchor={'middle'}>
+          A = blue, B = green
+        </text>
+
+        <path d={s1} stroke={'blue'} fill={'rgba(0, 0, 255, 0.25)'} />
+        <path d={s2} stroke={'green'} fill={'rgba(0, 255, 0, 0.25)'} />
+      </g>
+
+      {/* Clipped path */}
+      <g transform={'translate(200, 0)'}>
+        <text x={0} y={-80} width={200} textAnchor={'middle'}>
+          Clipped paths
+        </text>
+
+        <path d={s1} stroke={'gray'} fill={'rgba(195, 195, 195, 0.25)'} />
+        <path d={s2} stroke={'gray'} fill={'rgba(195, 195, 195, 0.25)'} />
+
+        {subject.map((s, idx) => {
+          const p = new Path(s.segment.start, s.segment.raw());
+          return (
+            <path
+              key={idx}
+              d={p.asSvgPath()}
+              stroke={idx % 2 === 0 ? 'red' : 'blue'}
+              fill={'none'}
+            />
+          );
+        })}
+
+        {subject
+          .filter(v => v.intersect)
+          .map((s, idx) => (
+            <circle key={idx} cx={s.point.x} cy={s.point.y} r={2} fill={'black'} />
+          ))}
+
+        {clip.map((s, idx) => {
+          const p = new Path(s.segment.start, s.segment.raw());
+          return (
+            <path
+              key={idx}
+              d={p.asSvgPath()}
+              stroke={idx % 2 === 0 ? 'green' : 'pink'}
+              fill={'none'}
+            />
+          );
+        })}
+      </g>
+
+      <g transform={'translate(0, 200)'}>
+        <text x={0} y={-80} width={200} textAnchor={'middle'}>
+          A union B
+        </text>
+
+        <path d={s1} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+        <path d={s2} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+
+        {aUnionB.all().map((p, idx) => (
+          <path key={idx} d={p.asSvgPath()} stroke={'red'} fill={'rgba(255, 0, 0, 0.25)'} />
+        ))}
+
+        {aUnionB.all().map(p => {
+          return p.segments.map((s, sidx) => (
+            <circle key={sidx} cx={s.start.x} cy={s.start.y} r={2} fill={'black'} />
+          ));
+        })}
+      </g>
+
+      <g transform={'translate(200, 200)'}>
+        <text x={0} y={-80} width={200} textAnchor={'middle'}>
+          A not B
+        </text>
+
+        <path d={s1} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+        <path d={s2} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+
+        {aNotB.all().map((p, idx) => (
+          <path key={idx} d={p.asSvgPath()} stroke={'red'} fill={'rgba(255, 0, 0, 0.25)'} />
+        ))}
+
+        {aNotB.all().map(p => {
+          return p.segments.map((s, sidx) => (
+            <circle key={sidx} cx={s.start.x} cy={s.start.y} r={2} fill={'black'} />
+          ));
+        })}
+      </g>
+
+      <g transform={'translate(400, 200)'}>
+        <text x={0} y={-80} width={200} textAnchor={'middle'}>
+          B not A
+        </text>
+
+        <path d={s1} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+        <path d={s2} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+
+        {bNotA.all().map((p, idx) => (
+          <path key={idx} d={p.asSvgPath()} stroke={'red'} fill={'rgba(255, 0, 0, 0.25)'} />
+        ))}
+
+        {bNotA.all().map(p => {
+          return p.segments.map((s, sidx) => (
+            <circle key={sidx} cx={s.start.x} cy={s.start.y} r={2} fill={'black'} />
+          ));
+        })}
+      </g>
+
+      <g transform={'translate(0, 400)'}>
+        <text x={0} y={-80} width={200} textAnchor={'middle'}>
+          A intersection B
+        </text>
+
+        <path d={s1} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+        <path d={s2} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+
+        {aIntersectionB.all().map((p, idx) => (
+          <path key={idx} d={p.asSvgPath()} stroke={'red'} fill={'rgba(255, 0, 0, 0.25)'} />
+        ))}
+
+        {aIntersectionB.all().map(p =>
+          p.segments.map((s, sidx) => {
+            return <circle key={sidx} cx={s.start.x} cy={s.start.y} r={2} fill={'black'} />;
+          })
+        )}
+      </g>
+
+      <g transform={'translate(200, 400)'}>
+        <text x={0} y={-80} width={200} textAnchor={'middle'}>
+          A XOR B
+        </text>
+
+        <path d={s1} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+        <path d={s2} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+
+        {aXorB.all().map((p, idx) => (
+          <path key={idx} d={p.asSvgPath()} stroke={'red'} fill={'rgba(255, 0, 0, 0.25)'} />
+        ))}
+
+        {aXorB.all().map(p => {
+          return p.segments.map((s, sidx) => (
+            <circle key={sidx} cx={s.start.x} cy={s.start.y} r={2} fill={'black'} />
+          ));
+        })}
+      </g>
+
+      <g transform={'translate(400, 400)'}>
+        <text x={0} y={-80} width={200} textAnchor={'middle'}>
+          A divide B
+        </text>
+
+        <path d={s1} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+        <path d={s2} stroke={'rgb(200, 200, 200)'} fill={'rgba(195, 195, 195, 0.25)'} />
+
+        {aDivideB.all().map((p, idx) => (
+          <path key={idx} d={p.asSvgPath()} stroke={'red'} fill={'rgba(255, 0, 0, 0.25)'} />
+        ))}
+
+        {aDivideB.all().map(p => {
+          return p.segments.map((s, sidx) => (
+            <circle key={sidx} cx={s.start.x} cy={s.start.y} r={2} fill={'black'} />
+          ));
+        })}
+      </g>
+    </svg>
+  );
+};
+
+const meta = {
+  title: 'Geometry/Path/boolean',
+  component: BooleanTest,
+  parameters: {
+    layout: 'centered'
+  },
+  argTypes: {}
+} satisfies Meta<typeof BooleanTest>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {}
+};
