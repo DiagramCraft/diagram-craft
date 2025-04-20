24180435198e2d15f2beffb153828b52b2bd68ac
Magnus Johansson <kkckkc@gmail.com>
2025-04-20 10:15:12 +0200

    chore[#163]: change to use vitest bench

---
+++ packages/geometry/src/bezier.bench.ts
@@ -0,0 +1,69 @@
+import { bench } from 'vitest';
+
+import { Random } from '@diagram-craft/utils/random';
+import { CubicBezier } from './bezier';
+import { Line } from './line';
+
+const r = new Random(123456);
+
+const randomPoint = (d: number) => {
+  return { x: r.nextFloat() * d, y: r.nextFloat() * d };
+};
+
+const dimension = 100;
+
+const opts = { time: 2000 };
+
+bench(
+  'lengthAt + tAtLength',
+  () => {
+    const b = new CubicBezier(
+      randomPoint(dimension),
+      randomPoint(dimension),
+      randomPoint(dimension),
+      randomPoint(dimension)
+    );
+
+    const t = r.nextFloat();
+    const p = b.lengthAtT(t);
+    b.tAtLength(p);
+  },
+  opts
+);
+
+bench(
+  'intersection-line',
+  () => {
+    const b = new CubicBezier(
+      randomPoint(dimension),
+      randomPoint(dimension),
+      randomPoint(dimension),
+      randomPoint(dimension)
+    );
+    const l = Line.of(randomPoint(dimension), randomPoint(dimension));
+
+    b.intersectsLine(l);
+  },
+  opts
+);
+
+bench(
+  ':intersection-bezier',
+  () => {
+    const b = new CubicBezier(
+      randomPoint(dimension),
+      randomPoint(dimension),
+      randomPoint(dimension),
+      randomPoint(dimension)
+    );
+    const b2 = new CubicBezier(
+      randomPoint(dimension),
+      randomPoint(dimension),
+      randomPoint(dimension),
+      randomPoint(dimension)
+    );
+
+    b.intersectsBezier(b2);
+  },
+  opts
+);
---
+++ packages/model/src/snap/snapManager.bench.ts
@@ -0,0 +1,50 @@
+import { bench } from 'vitest';
+
+import { Random } from '@diagram-craft/utils/random';
+import { DocumentBuilder } from '../diagram';
+import { DiagramDocument } from '../diagramDocument';
+import {
+  defaultEdgeRegistry,
+  defaultNodeRegistry
+} from '@diagram-craft/canvas-app/defaultRegistry';
+import { UnitOfWork } from '../unitOfWork';
+import { RegularLayer } from '../diagramLayer';
+import { DiagramNode } from '../diagramNode';
+
+const r = new Random(123456);
+
+const randomBox = () => {
+  return {
+    x: Math.round(r.nextFloat() * 1000),
+    y: Math.round(r.nextFloat() * 1000),
+    w: Math.round(r.nextFloat() * 100) + 1,
+    h: Math.round(r.nextFloat() * 100) + 1,
+    r: 0
+  };
+};
+
+const opts = { time: 2000 };
+
+const { diagram: d } = DocumentBuilder.empty(
+  '1',
+  '1',
+  new DiagramDocument(defaultNodeRegistry(), defaultEdgeRegistry())
+);
+
+UnitOfWork.execute(d, uow => {
+  for (let i = 0; i < 1000; i++) {
+    (d.activeLayer as RegularLayer).addElement(
+      new DiagramNode(i.toString(), 'rect', randomBox(), d, d.activeLayer, {}, {}),
+      uow
+    );
+  }
+});
+
+const snapManager = d.createSnapManager();
+bench(
+  'snapManager',
+  () => {
+    snapManager!.snapMove(randomBox());
+  },
+  opts
+);
