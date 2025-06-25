1e318a80cb3099628d9c056147b0444f81553929
Magnus Johansson <kkckkc@gmail.com>
2025-06-25 20:49:04 +0200

    Ensure autosave is not used for using shared CRDTs

--- packages/model/src/collaboration/collaborationConfig.ts
+++ packages/model/src/collaboration/collaborationConfig.ts
@@ -3,12 +3,14 @@ import { CRDTMap, CRDTRoot } from './crdt';
 import { NoOpCRDTMap, NoOpCRDTRoot } from './noopCrdt';
 
 export const CollaborationConfig: {
+  idNoOp: boolean;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   CRDTRoot: new (...args: any[]) => CRDTRoot;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   CRDTMap: new (...args: any[]) => CRDTMap<any>;
   Backend: CollaborationBackend;
 } = {
+  idNoOp: true,
   CRDTRoot: NoOpCRDTRoot,
   CRDTMap: NoOpCRDTMap,
   Backend: new NoOpCollaborationBackend()
