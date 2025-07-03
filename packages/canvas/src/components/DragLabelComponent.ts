fd769fda898474e40323e48ddc0516557ee6f2ed
Magnus Johansson <kkckkc@gmail.com>
2025-07-03 20:17:48 +0200

    Fix for packages/canvas

--- packages/canvas/src/components/DragLabelComponent.ts
+++ packages/canvas/src/components/DragLabelComponent.ts
@@ -2,7 +2,7 @@ import { DRAG_DROP_MANAGER, State } from '../dragDropManager';
 import { Component, createEffect } from '../component/component';
 import * as html from '../component/vdom-html';
 import { text } from '../component/vdom';
-import { CanvasState } from '../canvas/EditableCanvasComponent';
+import type { CanvasState } from '../canvas/EditableCanvasComponent';
 import { deepEquals } from '@diagram-craft/utils/object';
 import { hasElements } from '@diagram-craft/utils/array';
 
