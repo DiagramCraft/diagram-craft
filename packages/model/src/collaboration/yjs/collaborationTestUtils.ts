191120bc4ffa6b379829fe8061648b6604f4b5f6
Magnus Johansson <kkckkc@gmail.com>
2025-07-17 22:39:34 +0200

    Fix issue where undoing a move detaches node from edge

--- packages/model/src/collaboration/yjs/collaborationTestUtils.ts
+++ packages/model/src/collaboration/yjs/collaborationTestUtils.ts
@@ -4,6 +4,8 @@ import { NoOpCRDTMap, NoOpCRDTRoot } from '../noopCrdt';
 import { CollaborationConfig } from '../collaborationConfig';
 import { YJSMap, YJSRoot } from './yjsCrdt';
 import { vi } from 'vitest';
+import { TestModel } from '../../test-support/builder';
+import type { RegularLayer } from '../../diagramLayerRegular';
 
 export type Backend = {
   syncedDocs: () => [CRDTRoot, CRDTRoot | undefined];
@@ -50,4 +52,32 @@ export const Backends = {
       ]
     ];
   }
+};
+
+export const standardTestModel = (backend: Backend) => {
+  const [root1, root2] = backend.syncedDocs();
+
+  const diagram1 = TestModel.newDiagram(root1);
+  const doc1 = diagram1.document;
+  const layer1 = diagram1.newLayer();
+
+  const doc2 = root2 ? TestModel.newDocument(root2) : undefined;
+  const diagram2 = root2 ? doc2!.topLevelDiagrams[0]! : undefined;
+  const layer2 = root2 ? (diagram2!.layers.all[0] as RegularLayer) : undefined;
+
+  const elementChange = backend.createFns();
+  diagram1.on('elementChange', elementChange[0]);
+  if (diagram2) diagram2.on('elementChange', elementChange[1]);
+
+  return {
+    root1,
+    root2,
+    diagram1,
+    doc1,
+    layer1,
+    diagram2,
+    doc2,
+    layer2,
+    elementChange
+  };
 };
