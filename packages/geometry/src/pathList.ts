b823ff441d43c4296a9034086b52bad28c6dd981
Magnus Johansson <kkckkc@gmail.com>
2025-08-05 09:16:26 +0200

    Refactor PathList normalization

--- packages/geometry/src/pathList.ts
+++ packages/geometry/src/pathList.ts
@@ -1,9 +1,10 @@
 import { Path } from './path';
-import { assert, VERIFY_NOT_REACHED } from '@diagram-craft/utils/assert';
+import { assert } from '@diagram-craft/utils/assert';
 import { Box } from './box';
-import { MultiMap } from '@diagram-craft/utils/multimap';
 import { Point } from './point';
 import { LengthOffsetOnPath, TimeOffsetOnSegment } from './pathPosition';
+import { isDebug } from '@diagram-craft/utils/debug';
+import { classifyPathsAsHolesAndOutlines } from './pathListUtils';
 
 type ProjectedPointOnPathList = {
   offset: TimeOffsetOnSegment & LengthOffsetOnPath;
@@ -38,47 +39,30 @@ export class PathList {
   normalize() {
     // TODO: First we remove all self-intersections
 
-    // Secondly, we need to order all paths in a tree structure based on which contains which
-    const containedWithin = new MultiMap<number, number>();
-    for (let a = 0; a < this.paths.length; a++) {
-      for (let b = 0; b < this.paths.length; b++) {
-        if (a === b) continue;
-        const pa = this.paths[a];
-        const pb = this.paths[b];
-        if (pa.segments.map(s => s.start).every(p => pb.isInside(p) || pb.isOn(p))) {
-          containedWithin.add(a, b);
-        }
-      }
-    }
+    const classification = classifyPathsAsHolesAndOutlines(this.paths);
 
     const dest: Path[] = [];
 
-    let maxLoop = 100;
-    let state = true;
-    const queue = [...this.paths.map((_, i) => i)];
-    while (queue.length > 0) {
-      const removed: number[] = [];
-      for (const i of queue) {
-        const p = this.paths[i];
-        if (!containedWithin.has(i)) {
-          removed.push(i);
+    for (let depth = 0; depth < this.paths.length; depth++) {
+      let found = false;
+
+      for (const p of this.paths) {
+        const c = classification.get(p);
+        if (isDebug()) console.log('found', c);
+        if (c && c.depth === depth) {
+          const state = c?.type === 'outline';
 
           if (p.isClockwise() && state) dest.push(p.reverse());
           else if (!p.isClockwise() && !state) dest.push(p.reverse());
           else {
             dest.push(p);
           }
+
+          found = true;
         }
       }
 
-      removed.forEach(j => queue.splice(queue.indexOf(j), 1));
-      removed.forEach(r => queue.forEach(q => containedWithin.remove(q, r)));
-
-      state = !state;
-
-      if (maxLoop-- === 0) {
-        VERIFY_NOT_REACHED('Max loop reached');
-      }
+      if (!found) break;
     }
 
     this.paths = dest;
