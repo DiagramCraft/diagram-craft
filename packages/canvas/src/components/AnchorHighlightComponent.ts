ad0e2c484603018a2979ba15cbc4c042f5ba5c09
Magnus Johansson <kkckkc@gmail.com>
2025-09-09 18:10:55 +0200

    Improve support for anchors for isometric effect

--- packages/canvas/src/components/AnchorHighlightComponent.ts
+++ packages/canvas/src/components/AnchorHighlightComponent.ts
@@ -5,6 +5,7 @@ import { getHighlightValue, hasHighlight, Highlights } from '../highlight';
 import { Zoom } from './zoom';
 import { DiagramElement, isNode } from '@diagram-craft/model/diagramElement';
 import type { DiagramNode } from '@diagram-craft/model/diagramNode';
+import { getAnchorPosition } from '@diagram-craft/model/anchor';
 
 type Props = CanvasState;
 
@@ -37,12 +38,14 @@ export class AnchorHighlightComponent extends Component<Props> {
       ...[...this.highlightedElements].flatMap(element => {
         return element.anchors.map(anchor => {
           if (anchor.type === 'edge') {
+            const c1 = getAnchorPosition(element, anchor, 'start');
+            const c2 = getAnchorPosition(element, anchor, 'end');
             return svg.line({
               class: 'svg-node__anchor',
-              x1: element.bounds.x + anchor.start.x * element.bounds.w,
-              y1: element.bounds.y + anchor.start.y * element.bounds.h,
-              x2: element.bounds.x + anchor.end!.x * element.bounds.w,
-              y2: element.bounds.y + anchor.end!.y * element.bounds.h,
+              x1: c1.x,
+              y1: c1.y,
+              x2: c2.x,
+              y2: c2.y,
               style: `
                   stroke-width: ${z.str(2, 2)} !important; 
                   pointer-events: none; 
@@ -50,10 +53,11 @@ export class AnchorHighlightComponent extends Component<Props> {
                 `
             });
           } else {
+            const c = getAnchorPosition(element, anchor, 'start');
             return svg.circle({
               class: 'svg-node__anchor',
-              cx: element.bounds.x + anchor.start.x * element.bounds.w,
-              cy: element.bounds.y + anchor.start.y * element.bounds.h,
+              cx: c.x,
+              cy: c.y,
               r: z.str(4, 2),
               // TODO: Change this to be a class instead of a fixed color
               style: `pointer-events: none; fill: ${getHighlightValue(element, Highlights.NODE__ACTIVE_ANCHOR) === anchor.id ? 'var(--accent-9)' : 'var(--accent-3)'};`
