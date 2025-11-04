6e953269c83c1561780c1b710ba7049613b4b58d
Magnus Johansson <kkckkc@gmail.com>
2025-11-04 08:21:32 +0100

    Reduced duplication

--- packages/canvas-app/src/actions/exportAction.ts
+++ packages/canvas-app/src/actions/exportAction.ts
@@ -36,66 +36,73 @@ const downloadSVG = (svgData: string, filename = 'untitled.svg') => {
 const MARGIN = 50;
 const SCALE = 2;
 
+const prepareSvgForExport = async (context: ActionContext) => {
+  const bounds = Box.boundingBox(
+    context.model.activeDiagram.visibleElements().flatMap(e => {
+      return isEdge(e) ? [e.bounds, ...e.children.map(c => c.bounds)] : [e.bounds];
+    })
+  );
+
+  const clonedSvg = CanvasDomHelper.diagramElement(context.model.activeDiagram)!.cloneNode(
+    true
+  ) as HTMLElement;
+
+  const canvasFg = getComputedStyle(document.getElementById('app')!).getPropertyValue(
+    '--canvas-fg'
+  );
+  const canvasBg = getComputedStyle(document.getElementById('app')!).getPropertyValue(
+    '--canvas-bg'
+  );
+  const canvasBg2 = getComputedStyle(document.getElementById('app')!).getPropertyValue(
+    '--canvas-bg2'
+  );
+
+  clonedSvg.setAttribute(
+    'style',
+    `--canvas-fg: ${canvasFg}; --canvas-bg: ${canvasBg}; --canvas-bg2: ${canvasBg2};`
+  );
+  clonedSvg.setAttribute(
+    'viewBox',
+    `${bounds.x - MARGIN} ${bounds.y - MARGIN} ${bounds.w + 2 * MARGIN} ${bounds.h + 2 * MARGIN}`
+  );
+
+  // Cleanup some elements that should not be part of the export
+  clonedSvg.querySelector('.svg-doc-bounds')?.remove();
+  clonedSvg.querySelector('.svg-grid-container')?.remove();
+  clonedSvg.querySelectorAll('.svg-edge__backing').forEach(e => e.remove());
+
+  // Need to embed all object urls
+  for (const [, a] of context.model.activeDiagram.document.attachments.attachments) {
+    const dataUrl = await a.getDataUrl();
+    clonedSvg.querySelectorAll(`image[href="${a.url}"]`).forEach(e => {
+      e.setAttribute('href', dataUrl);
+    });
+  }
+
+  // Download and embed all external images
+  for (const [, img] of clonedSvg.querySelectorAll('image').entries()) {
+    const href = img.href.baseVal;
+    if (href.startsWith('data:')) continue;
+
+    console.log(`Downloading ${href}`);
+
+    const connection = await fetch(href);
+    const data = await connection.blob();
+    img.setAttribute('href', await blobToDataURL(data));
+
+    console.log('... done');
+  }
+
+  return { clonedSvg, bounds };
+};
+
 class ExportImageAction extends AbstractAction {
   execute(): void {
     const run = async () => {
-      const bounds = Box.boundingBox(
-        this.context.model.activeDiagram.visibleElements().flatMap(e => {
-          return isEdge(e) ? [e.bounds, ...e.children.map(c => c.bounds)] : [e.bounds];
-        })
-      );
+      const { clonedSvg, bounds } = await prepareSvgForExport(this.context);
 
-      const clonedSvg = CanvasDomHelper.diagramElement(this.context.model.activeDiagram)!.cloneNode(
-        true
-      ) as HTMLElement;
       clonedSvg.setAttribute('width', bounds.w.toString());
       clonedSvg.setAttribute('height', bounds.h.toString());
-
-      const canvasFg = getComputedStyle(document.getElementById('app')!).getPropertyValue(
-        '--canvas-fg'
-      );
-      const canvasBg = getComputedStyle(document.getElementById('app')!).getPropertyValue(
-        '--canvas-bg'
-      );
-      const canvasBg2 = getComputedStyle(document.getElementById('app')!).getPropertyValue(
-        '--canvas-bg2'
-      );
-
-      clonedSvg.setAttribute(
-        'style',
-        `--canvas-fg: ${canvasFg}; --canvas-bg: ${canvasBg}; --canvas-bg2: ${canvasBg2};`
-      );
-      clonedSvg.setAttribute(
-        'viewBox',
-        `${bounds.x - MARGIN} ${bounds.y - MARGIN} ${bounds.w + 2 * MARGIN} ${bounds.h + 2 * MARGIN}`
-      );
-
-      // Cleanup some elements that should not be part of the export
-      clonedSvg.querySelector('.svg-doc-bounds')?.remove();
-      clonedSvg.querySelector('.svg-grid-container')?.remove();
-      clonedSvg.querySelectorAll('.svg-edge__backing').forEach(e => e.remove());
-
-      // Need to embed all object urls
-      for (const [, a] of this.context.model.activeDiagram.document.attachments.attachments) {
-        const dataUrl = await a.getDataUrl();
-        clonedSvg.querySelectorAll(`image[href="${a.url}"]`).forEach(e => {
-          e.setAttribute('href', dataUrl);
-        });
-      }
-
-      // Download and embed all external images
-      for (const [, img] of clonedSvg.querySelectorAll('image').entries()) {
-        const href = img.href.baseVal;
-        if (href.startsWith('data:')) continue;
-
-        console.log(`Downloading ${href}`);
-
-        const connection = await fetch(href);
-        const data = await connection.blob();
-        img.setAttribute('href', await blobToDataURL(data));
-
-        console.log('... done');
-      }
 
       // TODO: Remove selection indicators etc
 
@@ -139,63 +146,10 @@ class ExportImageAction extends AbstractAction {
 class ExportSVGAction extends AbstractAction {
   execute(): void {
     const run = async () => {
-      const bounds = Box.boundingBox(
-        this.context.model.activeDiagram.visibleElements().flatMap(e => {
-          return isEdge(e) ? [e.bounds, ...e.children.map(c => c.bounds)] : [e.bounds];
-        })
-      );
+      const { clonedSvg, bounds } = await prepareSvgForExport(this.context);
 
-      const clonedSvg = CanvasDomHelper.diagramElement(this.context.model.activeDiagram)!.cloneNode(
-        true
-      ) as HTMLElement;
       clonedSvg.setAttribute('width', (bounds.w + 2 * MARGIN).toString());
       clonedSvg.setAttribute('height', (bounds.h + 2 * MARGIN).toString());
-
-      const canvasFg = getComputedStyle(document.getElementById('app')!).getPropertyValue(
-        '--canvas-fg'
-      );
-      const canvasBg = getComputedStyle(document.getElementById('app')!).getPropertyValue(
-        '--canvas-bg'
-      );
-      const canvasBg2 = getComputedStyle(document.getElementById('app')!).getPropertyValue(
-        '--canvas-bg2'
-      );
-
-      clonedSvg.setAttribute(
-        'style',
-        `--canvas-fg: ${canvasFg}; --canvas-bg: ${canvasBg}; --canvas-bg2: ${canvasBg2};`
-      );
-      clonedSvg.setAttribute(
-        'viewBox',
-        `${bounds.x - MARGIN} ${bounds.y - MARGIN} ${bounds.w + 2 * MARGIN} ${bounds.h + 2 * MARGIN}`
-      );
-
-      // Cleanup some elements that should not be part of the export
-      clonedSvg.querySelector('.svg-doc-bounds')?.remove();
-      clonedSvg.querySelector('.svg-grid-container')?.remove();
-      clonedSvg.querySelectorAll('.svg-edge__backing').forEach(e => e.remove());
-
-      // Need to embed all object urls
-      for (const [, a] of this.context.model.activeDiagram.document.attachments.attachments) {
-        const dataUrl = await a.getDataUrl();
-        clonedSvg.querySelectorAll(`image[href="${a.url}"]`).forEach(e => {
-          e.setAttribute('href', dataUrl);
-        });
-      }
-
-      // Download and embed all external images
-      for (const [, img] of clonedSvg.querySelectorAll('image').entries()) {
-        const href = img.href.baseVal;
-        if (href.startsWith('data:')) continue;
-
-        console.log(`Downloading ${href}`);
-
-        const connection = await fetch(href);
-        const data = await connection.blob();
-        img.setAttribute('href', await blobToDataURL(data));
-
-        console.log('... done');
-      }
 
       const svgData = new XMLSerializer().serializeToString(clonedSvg);
       downloadSVG(svgData, 'diagram.svg');
