7e713bb788554ab2aa54c54b3d2005052741eaa8
Magnus Johansson <kkckkc@gmail.com>
2025-12-05 10:25:24 +0100

    Revert some unrelated changes

--- packages/geometry/src/path.bench.ts
+++ packages/geometry/src/path.bench.ts
@@ -1,13 +1,42 @@
 import { bench } from 'vitest';
 
+import { Random } from '@diagram-craft/utils/random';
+import { PathListBuilder } from './pathListBuilder';
+import { Path } from './path';
+
+const r = new Random(123456);
+
+const randomPoint = (d: number) => {
+  return { x: r.nextFloat() * d, y: r.nextFloat() * d };
+};
+
+const randomPath = (d: number): Path => {
+  const length = r.nextRange(1, 7);
+  const builder = new PathListBuilder();
+  builder.moveTo(randomPoint(d));
+
+  for (let i = 0; i < length; i++) {
+    if (r.nextBoolean()) {
+      builder.lineTo(randomPoint(d));
+    } else {
+      builder.cubicTo(randomPoint(d), randomPoint(d), randomPoint(d));
+    }
+  }
+  return builder.getPaths().singular();
+};
+
+const dimension = 100;
+
 const opts = { time: 2000 };
 
 bench(
   'intersection',
   () => {
-    let a = 0;
     for (let i = 0; i < 100; i++) {
-      a++;
+      const p1 = randomPath(dimension);
+      const p2 = randomPath(dimension);
+
+      p1.intersections(p2);
     }
   },
   opts
