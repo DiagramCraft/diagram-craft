cbfad47adc62a757c4369a759a92b32377f59748
Magnus Johansson <kkckkc@gmail.com>
2025-12-17 14:05:56 +0100

    Adding color panel

--- packages/main/src/react-app/toolwindow/StyleOverviewToolWindow/StyleOverviewToolWindow.tsx
+++ packages/main/src/react-app/toolwindow/StyleOverviewToolWindow/StyleOverviewToolWindow.tsx
@@ -1,11 +1,15 @@
 import { ToolWindow } from '../ToolWindow';
 import { FontsTab } from './FontsTab';
+import { ColorsTab } from './ColorsTab';
 
 export const StyleOverviewToolWindow = () => {
   return (
     <ToolWindow.Root id={'style-overview'} defaultTab={'fonts'}>
       <ToolWindow.Tab title={'Fonts'} id={'fonts'}>
         <FontsTab />
+      </ToolWindow.Tab>
+      <ToolWindow.Tab title={'Colors'} id={'colors'}>
+        <ColorsTab />
       </ToolWindow.Tab>
     </ToolWindow.Root>
   );
