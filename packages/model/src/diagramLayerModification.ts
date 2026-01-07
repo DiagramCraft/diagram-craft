aafa1a55aaed7c47c48f125e2c2db6281be4ee84
Magnus Johansson <kkckkc@gmail.com>
2026-01-07 08:18:21 +0100

    ModificationLayer

--- packages/model/src/diagramLayerModification.ts
+++ packages/model/src/diagramLayerModification.ts
@@ -171,52 +171,49 @@ export class ModificationLayer extends Layer<ModificationLayer> {
 
   removeElement(el: DiagramElement, uow: UnitOfWork) {
     this.modifyRemove(el.id, uow);
+    uow.removeElement(el, this, this.#modifications.size - 1);
   }
 
   modifyRemove(id: string, uow: UnitOfWork) {
-    uow.snapshot(this);
-    this.#modifications.add(id, { id, type: ModificationType.Remove });
+    uow.executeUpdate(this, () => {
+      this.#modifications.add(id, { id, type: ModificationType.Remove });
+    });
   }
 
   modifyAdd(id: string, el: DiagramElement, uow: UnitOfWork) {
     assert.true(el instanceof DelegatingDiagramNode || el instanceof DelegatingDiagramEdge);
 
-    uow.snapshot(this);
-    this.#modifications.add(id, { id, type: ModificationType.Add, element: el });
+    uow.executeUpdate(this, () => {
+      this.#modifications.add(id, { id, type: ModificationType.Add, element: el });
 
-    this.processElementForAdd(el);
+      this.processElementForAdd(el);
 
-    uow.addElement(el, this, this.#modifications.size - 1);
-    uow.updateElement(this);
+      uow.addElement(el, this, this.#modifications.size - 1);
+    });
   }
 
   modifyChange(id: string, el: DiagramElement, uow: UnitOfWork) {
     assert.true(el instanceof DelegatingDiagramNode || el instanceof DelegatingDiagramEdge);
 
-    uow.snapshot(this);
-    this.#modifications.add(id, { id, type: ModificationType.Change, element: el });
+    uow.executeUpdate(this, () => {
+      this.#modifications.add(id, { id, type: ModificationType.Change, element: el });
 
-    this.processElementForAdd(el);
-
-    uow.addElement(el, this, this.#modifications.size - 1);
-    uow.updateElement(this);
+      this.processElementForAdd(el);
+    });
   }
 
   clearModification(id: string, uow: UnitOfWork) {
-    uow.snapshot(this);
+    uow.executeUpdate(this, () => {
+      const m = mustExist(this.#modifications.get(id));
+      this.#modifications.remove(id);
 
-    const m = mustExist(this.#modifications.get(id));
-    this.#modifications.remove(id);
-
-    if (m.type === ModificationType.Add) {
-      uow.snapshot(m.element!);
-      uow.removeElement(m.element!, this, this.#modifications.size - 1);
-    } else if (m.type === ModificationType.Change || m.type === ModificationType.Remove) {
-      const el = this.diagram.lookup(id)!;
-      uow.snapshot(el);
-      uow.updateElement(el);
-    }
-    uow.updateElement(this);
+      if (m.type === ModificationType.Add) {
+        uow.removeElement(m.element!, this, this.#modifications.size - 1);
+      } else if (m.type === ModificationType.Change || m.type === ModificationType.Remove) {
+        const el = this.diagram.lookup(id)!;
+        uow.updateElement(el);
+      }
+    });
   }
 
   getModification(id: string): Modification | undefined {
