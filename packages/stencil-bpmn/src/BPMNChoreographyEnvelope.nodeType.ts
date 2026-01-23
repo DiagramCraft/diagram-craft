4a3d13a31ba910249a9ef69ac7d7b0c8ff3d2ef9
Magnus Johansson <kkckkc@gmail.com>
2026-01-23 11:47:07 +0100

    Cleanup rendering of BPMNChoreographyEnvelopeNodeDefinition

--- packages/stencil-bpmn/src/BPMNChoreographyEnvelope.nodeType.ts
+++ packages/stencil-bpmn/src/BPMNChoreographyEnvelope.nodeType.ts
@@ -6,7 +6,6 @@ import {
 import { ShapeBuilder } from '@diagram-craft/canvas/shape/ShapeBuilder';
 import { PathListBuilder } from '@diagram-craft/geometry/pathListBuilder';
 import { _p } from '@diagram-craft/geometry/point';
-import { DiagramNode } from '@diagram-craft/model/diagramNode';
 
 declare global {
   namespace DiagramCraft {
@@ -32,43 +31,15 @@ export class BPMNChoreographyEnvelopeNodeDefinition extends ShapeNodeDefinition 
       // Draw the envelope rectangle
       shapeBuilder.boundaryPath(this.def.getBoundingPathBuilder(props.node).getPaths().all());
 
-      // Draw the envelope flap (triangle at the top)
-      const flapPath = new PathListBuilder()
+      // Draw the V-shape from the top
+      const vShapePath = new PathListBuilder()
         .moveTo(_p(bounds.x, bounds.y))
         .lineTo(_p(bounds.x + bounds.w / 2, bounds.y + bounds.h / 2))
         .lineTo(_p(bounds.x + bounds.w, bounds.y));
 
-      shapeBuilder.path(flapPath.getPaths().all(), undefined, {
-        style: { fill: 'none' }
-      });
-
-      // Draw the diagonal lines from bottom corners to center
-      const leftDiagonal = new PathListBuilder()
-        .moveTo(_p(bounds.x, bounds.y + bounds.h))
-        .lineTo(_p(bounds.x + bounds.w / 2, bounds.y + bounds.h / 2));
-
-      shapeBuilder.path(leftDiagonal.getPaths().all(), undefined, {
-        style: { fill: 'none' }
-      });
-
-      const rightDiagonal = new PathListBuilder()
-        .moveTo(_p(bounds.x + bounds.w, bounds.y + bounds.h))
-        .lineTo(_p(bounds.x + bounds.w / 2, bounds.y + bounds.h / 2));
-
-      shapeBuilder.path(rightDiagonal.getPaths().all(), undefined, {
+      shapeBuilder.path(vShapePath.getPaths().all(), undefined, {
         style: { fill: 'none' }
       });
     }
   };
-
-  getBoundingPathBuilder(def: DiagramNode) {
-    const bounds = def.bounds;
-
-    return new PathListBuilder()
-      .moveTo(_p(bounds.x, bounds.y))
-      .lineTo(_p(bounds.x + bounds.w, bounds.y))
-      .lineTo(_p(bounds.x + bounds.w, bounds.y + bounds.h))
-      .lineTo(_p(bounds.x, bounds.y + bounds.h))
-      .close();
-  }
 }
