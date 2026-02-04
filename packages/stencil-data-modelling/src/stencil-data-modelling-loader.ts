e974dd5d85de0b56bf882b89e2f909b772e214cf
Magnus Johansson <kkckkc@gmail.com>
2026-02-04 15:23:42 +0100

    Initial support for Barker notation

--- packages/stencil-data-modelling/src/stencil-data-modelling-loader.ts
+++ packages/stencil-data-modelling/src/stencil-data-modelling-loader.ts
@@ -6,13 +6,18 @@ import {
 } from '@diagram-craft/model/elementDefinitionRegistry';
 import chenStencils from './chen/chen-stencils.yaml';
 import ieStencils from './ie/ie-stencils.yaml';
+import barkerStencils from './barker/barker-stencils.yaml';
 import { loadStencilsFromYaml } from '@diagram-craft/model/elementDefinitionLoader';
 import { IEEntityTextNodeDefinition } from '@diagram-craft/stencil-data-modelling/ie/IEEntityText.nodeType';
 import { IEEntityNodeDefinition } from '@diagram-craft/stencil-data-modelling/ie/IEEntity.nodeType';
+import { BarkerEntityTextNodeDefinition } from '@diagram-craft/stencil-data-modelling/barker/BarkerEntityText.nodeType';
+import { BarkerEntityNodeDefinition } from '@diagram-craft/stencil-data-modelling/barker/BarkerEntity.nodeType';
 
 export const registerDataModellingNodes = async (nodes: NodeDefinitionRegistry) => {
   nodes.register(new IEEntityTextNodeDefinition());
   nodes.register(new IEEntityNodeDefinition());
+  nodes.register(new BarkerEntityNodeDefinition());
+  nodes.register(new BarkerEntityTextNodeDefinition());
 };
 
 export const registerDataModellingEdges = async (_edges: EdgeDefinitionRegistry) => {};
@@ -52,5 +57,14 @@ export const registerDataModellingStencils = async (registry: Registry) => {
   loadStencilsFromYaml(ieStencils).forEach(s => {
     dataModellingStencils.stencils.push(s);
     dataModellingStencils.subPackages!.find(p => p.id === 'ie')?.stencils.push(s);
+  });
+
+  /* *********************************************************************** */
+  /* BARKER PACKAGE                                                          */
+  /* *********************************************************************** */
+
+  loadStencilsFromYaml(barkerStencils).forEach(s => {
+    dataModellingStencils.stencils.push(s);
+    dataModellingStencils.subPackages!.find(p => p.id === 'barker')?.stencils.push(s);
   });
 };
