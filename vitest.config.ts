0941cc7ef9d5586feff5f341c5136167595719bc
Magnus Johansson <kkckkc@gmail.com>
2026-02-04 13:37:04 +0100

    chore/upgrade vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,11 +15,6 @@ export default defineConfig({
     },
     css: false,
     pool: 'threads',
-    poolOptions: {
-      threads: {
-        useAtomics: true
-      }
-    },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'json-summary'],
