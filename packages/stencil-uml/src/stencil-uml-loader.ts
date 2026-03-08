c67cdaf5ce248d49ae2d3b717cd91d1268e6f8f4
Magnus Johansson <kkckkc@gmail.com>
2026-03-08 20:39:31 +0100

    Creating UMLClassTemplate

--- packages/stencil-uml/src/stencil-uml-loader.ts
+++ packages/stencil-uml/src/stencil-uml-loader.ts
@@ -1,11 +1,13 @@
 import { NodeDefinitionRegistry, Registry } from '@diagram-craft/model/elementDefinitionRegistry';
 import { StencilPackage } from '@diagram-craft/model/stencilRegistry';
 import { UMLClassNodeDefinition } from '@diagram-craft/stencil-uml/class/UMLClass.nodeType';
+import { UMLClassTemplateNodeDefinition } from '@diagram-craft/stencil-uml/class/UMLClassTemplate.nodeType';
 import { loadStencilsFromYaml } from '@diagram-craft/model/elementDefinitionLoader';
 import classStencils from './class/uml-class-stencils.yaml';
 
 export const registerUMLNodes = async (nodes: NodeDefinitionRegistry) => {
   nodes.register(new UMLClassNodeDefinition());
+  nodes.register(new UMLClassTemplateNodeDefinition());
 };
 
 export const loadUMLStencils = async (registry: Registry) => {
