65c7d93921371f7ea2b052cc261da122203ec290
Magnus Johansson <kkckkc@gmail.com>
2026-03-14 09:22:35 +0100

    fix/help message cannot be closed with X button

--- packages/main/src/react-app/actions/toggleHelp.ts
+++ packages/main/src/react-app/actions/toggleHelp.ts
@@ -1,4 +1,4 @@
-import { AbstractToggleAction, ActionContext } from '@diagram-craft/canvas/action';
+import { AbstractToggleAction, ActionContext, ActionCriteria } from '@diagram-craft/canvas/action';
 import { UserState } from '../../UserState';
 import { $tStr } from '@diagram-craft/utils/localize';
 
@@ -14,35 +14,15 @@ export const toggleHelpActions = (context: ActionContext) => ({
 
 export class ToggleHelpAction extends AbstractToggleAction {
   name = $tStr('action.TOGGLE_HELP.name', 'Toggle Help');
-  private userState: UserState;
 
-  constructor(context: ActionContext) {
-    super(context);
-
-    this.userState = UserState.get();
-    this.state = this.userState.showHelp;
-
-    setTimeout(() => {
-      document.getElementById('help')!.style.opacity = this.state ? '100' : '0';
-
-      if (!this.state) {
-        document.getElementById('help')!.style.display = 'none';
-      }
-    }, 200);
+  getStateCriteria() {
+    return ActionCriteria.EventTriggered(UserState.get(), 'change', () => {
+      return UserState.get().showHelp ?? true;
+    });
   }
 
   execute(): void {
-    if (this.state) {
-      document.getElementById('help')!.style.opacity = '0';
-      setTimeout(() => (document.getElementById('help')!.style.display = 'none'), 500);
-      this.state = false;
-    } else {
-      document.getElementById('help')!.style.opacity = '100';
-      document.getElementById('help')!.style.display = 'block';
-      this.state = true;
-    }
-
-    this.userState.showHelp = this.state;
-    this.emit('actionChanged');
+    UserState.get().showHelp = !this.state;
+    this.emit('actionTriggered');
   }
 }
--- packages/main/src/react-app/components/HelpMessage.tsx
+++ packages/main/src/react-app/components/HelpMessage.tsx
@@ -3,20 +3,34 @@ import { useRedraw } from '../hooks/useRedraw';
 import { TbX } from 'react-icons/tb';
 import styles from './HelpMessage.module.css';
 import { Button } from '@diagram-craft/app-components/Button';
-import { useApplication } from '../../application';
 import { HelpState } from '../HelpState';
+import { UserState } from '../../UserState';
+import { useApplication } from '../../application';
 
 export const HelpMessage = (props: Props) => {
   const redraw = useRedraw();
   const application = useApplication();
-  useEventListener(props.helpState, 'helpChange', () => {
-    redraw();
-  });
+
+  useEventListener(props.helpState, 'helpChange', redraw);
+  useEventListener(UserState.get(), 'change', redraw);
+
+  const help = props.helpState.help;
+  if (!help || !UserState.get().showHelp) {
+    return null;
+  }
 
   return (
     <div id="help" className={styles.icHelp}>
-      <div>{props.helpState.help?.message}</div>
-      <Button variant={'icon-only'} onClick={() => application.actions['TOGGLE_HELP']?.execute()}>
+      <div>{help.message}</div>
+      <Button
+        variant={'icon-only'}
+        onMouseDown={event => {
+          // This is to prevent conflict with dnd event handling
+          event.preventDefault();
+          event.stopPropagation();
+        }}
+        onClick={() => application.actions['TOGGLE_HELP']?.execute()}
+      >
         <TbX />
       </Button>
     </div>
