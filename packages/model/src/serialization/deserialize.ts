0484b9101addd38266c96cea2ca3a5849c2a341b
Magnus Johansson <kkckkc@gmail.com>
2026-04-01 15:14:48 +0200

    Promote diagram views to a collaborative manager

--- packages/model/src/serialization/deserialize.ts
+++ packages/model/src/serialization/deserialize.ts
@@ -575,7 +575,9 @@ const deserializeDiagrams = async <T extends Diagram>(
       }
 
       if ($d.views) {
-        newDiagram.views.push(...$d.views);
+        for (const view of $d.views) {
+          newDiagram.views.add(view.name, { id: view.id, layers: [...view.layers] });
+        }
       }
 
       if ($d.zoom) {
