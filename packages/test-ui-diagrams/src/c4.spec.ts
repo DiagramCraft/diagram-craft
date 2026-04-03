a2bca68f0594e8f6482acc718c22c060345914ea
Magnus Johansson <kkckkc@gmail.com>
2026-04-03 15:48:57 +0200

    Move some local tests (as they are no longer local)

---
+++ packages/test-ui-diagrams/src/c4.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+import { downloadDiagram, setTab, waitForApplicationLoaded } from './testUtils';
+
+test('tab 1 screenshot', async ({ page }) => {
+  await page.goto('http://localhost:5173?crdtClear=true#/C4.json');
+  await waitForApplicationLoaded(page);
+  await setTab(page, 0);
+  expect(await downloadDiagram(page)).toMatchSnapshot();
+});
+
+test('tab2.screenshot', async ({ page }) => {
+  await page.goto('http://localhost:5173?crdtClear=true#/C4.json');
+  await waitForApplicationLoaded(page);
+  await setTab(page, 1);
+  expect(await downloadDiagram(page)).toMatchSnapshot();
+});
+
+test('tab3.screenshot', async ({ page }) => {
+  await page.goto('http://localhost:5173?crdtClear=true#/C4.json');
+  await waitForApplicationLoaded(page);
+  await setTab(page, 2);
+  expect(await downloadDiagram(page)).toMatchSnapshot();
+});
