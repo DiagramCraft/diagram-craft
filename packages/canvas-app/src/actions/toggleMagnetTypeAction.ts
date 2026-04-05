1e72bc83455b9c4c96c2a20a04dd91c6a8bb0514
Magnus Johansson <kkckkc@gmail.com>
2026-04-05 10:19:41 +0200

    fix/snap settings leaking across diagrams

--- packages/canvas-app/src/actions/toggleMagnetTypeAction.ts
+++ packages/canvas-app/src/actions/toggleMagnetTypeAction.ts
@@ -1,6 +1,6 @@
 import { AbstractToggleAction, ActionContext, ActionCriteria } from '@diagram-craft/canvas/action';
 import { MagnetType } from '@diagram-craft/canvas/snap/magnet';
-import { DEFAULT_SNAP_CONFIG, getSnapConfig } from '@diagram-craft/canvas/snap/snapManager';
+import { createSnapConfig, getSnapConfig } from '@diagram-craft/canvas/snap/snapManager';
 import { $tStr, type TranslatedString } from '@diagram-craft/utils/localize';
 import { UnitOfWork } from '@diagram-craft/model/unitOfWork';
 
@@ -62,7 +62,7 @@ export class ToggleMagnetTypeAction extends AbstractToggleAction {
   execute(): void {
     UnitOfWork.executeWithUndo(this.context.model.activeDiagram, 'Toggle magnet type', uow =>
       this.context.model.activeDiagram.updateProps(p => {
-        p.snap ??= DEFAULT_SNAP_CONFIG;
+        p.snap = createSnapConfig(p.snap);
         p.snap.magnetTypes[this.magnetType] = !this.state;
       }, uow)
     );
