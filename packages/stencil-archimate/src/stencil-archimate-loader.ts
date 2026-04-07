17f1b3d040ca737dafcfa311f8673f07c80121ec
Magnus Johansson <kkckkc@gmail.com>
2026-04-07 15:54:32 +0200

    Initial Archimate stencils

--- packages/stencil-archimate/src/stencil-archimate-loader.ts
+++ packages/stencil-archimate/src/stencil-archimate-loader.ts
@@ -1,18 +1,28 @@
 import { Registry } from '@diagram-craft/model/elementDefinitionRegistry';
 import { StencilPackage } from '@diagram-craft/model/stencilRegistry';
 import { YamlStencilLoader } from '@diagram-craft/canvas/yamlStencilLoader';
-import archimateStencils from './archimate-stencils.yaml';
+import archimateApplicationStencils from './archimate-application-stencils.yaml';
+import archimateBusinessStencils from './archimate-business-stencils.yaml';
+import archimateStyles from './archimate-styles.yaml';
+import archimateTechnologyStencils from './archimate-technology-stencils.yaml';
 
 export const loadArchimateStencils = async (_registry: Registry) => {
   const archimateStencilPkg: StencilPackage = {
     id: 'archimate',
     stencils: [],
     type: 'default',
-    subPackages: [{ id: 'main', name: 'Archimate', stencils: [] }]
+    subPackages: [
+      { id: 'business', name: 'Business', stencils: [] },
+      { id: 'application', name: 'Application', stencils: [] },
+      { id: 'technology', name: 'Technology', stencils: [] }
+    ]
   };
 
   const loader = new YamlStencilLoader(archimateStencilPkg);
-  loader.registerSubPackage('main', archimateStencils);
+  loader.registerStyles(archimateStyles);
+  loader.registerSubPackage('business', archimateBusinessStencils);
+  loader.registerSubPackage('application', archimateApplicationStencils);
+  loader.registerSubPackage('technology', archimateTechnologyStencils);
 
   return loader.apply();
 };
