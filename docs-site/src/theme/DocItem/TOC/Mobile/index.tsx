3d359afc980233d45eb93da336c850509163236e
Magnus Johansson <kkckkc@gmail.com>
2026-07-03 14:02:06 +0200

    feat[docs]/related readings in sidebar

---
+++ docs-site/src/theme/DocItem/TOC/Mobile/index.tsx
@@ -0,0 +1,16 @@
+import React, {type ReactNode} from 'react';
+import Mobile from '@theme-original/DocItem/TOC/Mobile';
+import type MobileType from '@theme/DocItem/TOC/Mobile';
+import type {WrapperProps} from '@docusaurus/types';
+import RelatedReading from '@site/src/components/RelatedReading';
+
+type Props = WrapperProps<typeof MobileType>;
+
+export default function MobileWrapper(props: Props): ReactNode {
+  return (
+    <>
+      <Mobile {...props} />
+      <RelatedReading />
+    </>
+  );
+}
