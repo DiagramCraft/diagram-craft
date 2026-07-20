b7dcb0e7d6a6f1918aeba3a18e016431c115bf68
Magnus Johansson <kkckkc@gmail.com>
2026-07-20 14:06:41 +0200

    Remove tests

--- arch-register-packages/web/src/sections/markdown/debugast.test.ts
+++
@@ -1,7 +0,0 @@
-import { test } from 'vitest';
-import { parseMarkdownWithComponents } from './preview/mdxMarkdown';
-
-test('debug', () => {
-  const nodes = parseMarkdownWithComponents('Before\n\n<div class="old">raw</div>\n\nAfter');
-  console.log(JSON.stringify(nodes, null, 2));
-});
